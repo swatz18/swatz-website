2aaca38e6f77e4e414894c9ce71dd41d6689774c
swathi <swathihsnk26@gmail.com>
2026-08-10 12:11:09 +0530

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    base: '/swatz-website/',
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
 })
