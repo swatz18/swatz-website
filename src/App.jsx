83d05bda3324a7b89bef826d1e404bd445a0ba19
swathi <swathihsnk26@gmail.com>
2026-08-16 20:15:04 +0530

    Update routing for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     return (
         <CartProvider>
            
-             <BrowserRouter basename="/swatz-website">
+             <BrowserRouter>
                 <>
                     <ScrollToTop />
                     <Routes>
