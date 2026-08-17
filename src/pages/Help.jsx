ab955cacde31a7801962684f843a021263318846
swathi <swathihsnk26@gmail.com>
2026-08-18 00:55:18 +0530

    updated no

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -378,7 +378,7 @@ export default function Help() {
                     </p>
 
                     <a
-                        href="https://wa.me/917502131997"
+                        href="https://wa.me/919489771802"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="help-whatsapp-button"
