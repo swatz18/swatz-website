c36f0d9710313d76fadaa613ebb93255349b0807
swathi <swathihsnk26@gmail.com>
2026-08-18 01:13:23 +0530

    updated no

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -128,7 +128,7 @@ export default function Footer() {
                         </a>
 
                         <a
-                            href="https://wa.me/917502131997"
+                            href="https://wa.me/919489771802"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
